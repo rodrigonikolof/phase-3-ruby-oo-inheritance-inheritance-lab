69a7a457b9ddae7f1229e20d039e671f444a4af2
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2023-01-21 15:27:38 +1100

    commit tree a93f285393cbd4a305f24aadab7e4a158d498d47

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative "../lib/user"
+require_relative "../lib/teacher"
+require_relative "../lib/student"
+
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize
+    @knowledge = []
+    end
+
+    def learn string 
+        @knowledge << string
+    end
+
 
 end
